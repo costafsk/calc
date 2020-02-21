8ba09985718886f8ee60b79a3771a3f2b69ad87a
Diego Costa <diego.machado.d.costa@gmail.com>
2020-02-21 10:46:24 -0300

    initial

---
+++ src/class/Calculator.php
@@ -0,0 +1,13 @@
+<?php
+
+  class Calculator {
+    construct () {
+
+    }
+
+    calc (expression) {
+      
+    }
+  }
+
+?>
---
+++ src/view/index.php
@@ -0,0 +1,40 @@
+<?php require_once('./includes/header.php') ?>
+
+<main>
+  <div class="calculator d-flex flex-column align-items-column">
+    <div class="display">
+    </div>
+    <div class="d-flex flex-row">
+      <div class="btn-number-column">
+        <div>
+          <div class="btn-number">9</div>
+          <div class="btn-number">8</div>
+          <div class="btn-number">7</div>
+        </div>
+        <div>
+          <div class="btn-number">6</div>
+          <div class="btn-number">5</div>
+          <div class="btn-number">4</div>
+        </div>
+        <div>
+          <div class="btn-number">3</div>
+          <div class="btn-number">2</div>
+          <div class="btn-number">1</div>
+        </div>
+        <div>
+          <div class="btn-number">0</div>
+          <div class="btn-number">.</div>
+          <div class="btn-number">=</div>
+        </div>
+      </div>
+      <div class="btn-operator-column">
+        <div class="btn-operator">+</div>
+        <div class="btn-operator">-</div>
+        <div class="btn-operator">/</div>
+        <div class="btn-operator">*</div>
+      </div>
+    </div>
+  </div>
+</main>
+
+<?php require_once('./includes/footer.php') ?>
